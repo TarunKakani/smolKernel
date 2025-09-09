2c5b7ad3301122627dd74a53f6088faba583a29c
TarunKakani <tarunkakani007@gmail.com>
2025-09-09 18:27:56 +0530

    finished first implementation of small kernel

--- kernel.asm
+++ kernel.asm
@@ -1,12 +1,18 @@
 ;;kernel.asm
+
 bits 32		;nasm directive - 32 bit
 section .text
+        ;multiboot spec header
+        align 4
+        dd 0x1BADB002     ;magic field number
+        dd 0x00           ;flags field will be set to zero
+        dd - (0x1BADB002 + 0x00)    ;this is the checksum. m + f + c should be zero
 
 global start
 extern kmain	;kmain is defined in the c file
 
 start:
-	cli	;block interrupts
+	cli	;blocks interrupts/ short for clear interrupt
 	mov esp, stack_space	;set stack pointer
 	call kmain
 	hlt
@@ -14,4 +20,3 @@ start:
 section .bss
 resb 8192	;8KB for stack
 stack_space:
-
--- kernel.c
+++ kernel.c
@@ -1,3 +1,37 @@
-void main(void){
-	const char *str = "my first kernel";
+// all this is doing is clearing the screen and write to it the string my first kernel 
+// the video memory declared at the start is the start of video memory in protected mode
 
+// now to understand video memory
+//
+
+void kmain(void){
+	const char *str = "my first kernel";
+  char *vidptr = (char*)0xb8000; // video memory begins from here
+  unsigned int i = 0;
+  unsigned int j = 0;
+
+  // the below loop clears the screen (???)
+  while(j < 80 * 25 * 2){
+    vidptr[j] = ' ';
+
+    // attribute byte light grey on black screen
+    vidptr[j+1] = 0x07;
+    j = j + 2;
+  }
+
+  j = 0;
+  
+  // this loop will write the string to video memory
+  while(str[j] != '\0'){
+
+    // character ascii
+    vidptr[i] = str[j];
+    
+    // give character black bg and light grey fg
+    vidptr[i+1] = 0x07;
+    ++j;
+    i = i + 2;
+  }
+
+  return;
+}
